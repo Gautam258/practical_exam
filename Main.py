37b5b0ba936af4736cf18973601cd294dc015414
Gautam258 <89130140+Gautam258@users.noreply.github.com>
2024-04-16 09:03:07 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
-def Add(a,b):
-  return a+b
-def display():
-  print("hello world")
 
-if __name__=__main__:
-  Display()
+def Add(a,b): 
+
+ return a+b 
+
+def Display(): 
+
+ print("Hello") 
+
+if __name__=='__main__': 
+
+ Display() 
+
